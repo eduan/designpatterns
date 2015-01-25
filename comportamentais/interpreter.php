36aa361cfa5ddef77fc6a0b850424b42bafad9ea
eduan <eduanlenine@yahoo.com.br>
2015-01-25 14:01:12 +0000

    interpreter.php edited online with Bitbucket

--- comportamentais/interpreter.php
+++ comportamentais/interpreter.php
@@ -64,6 +64,45 @@ class VariableExpression extends Expression{
     }
 }
 
+abstract class OperatorExpression extends Expression{
+    protected $lOp;
+    protected $rOp;
+     
+    function __construt(Expression $lOp, Expression $rOp){
+        $this->lOp = $lOp;
+        $this->rOp = $rOp;
+    }
+    
+    function interpret(InterpreterContext $context){
+        $this->lOp->interpret($context);
+        $this->rOp->interpret($context);
+        $resultL = $context->lookup($this->lOp);
+        $resultR = $context->lookup($this->rOp);
+        $this->doInterpret($context, $resultL, $resultR);
+    }
+    
+     protected abstract function doInterpret(InterpreterContext $context, $resultL, $resultR);
+     
+}
+
+class EqualsExpression extends OperatorExpression{
+     protected function doInterpret(InterpreterContext $context, $resultL, $resultR){
+         $context->replace($this, $resultL == $resultR);
+     }
+}
+
+class OrExpression extends OperatorExpression{
+     protected function doInterpret(InterpreterContext $context, $resultL, $resultR){
+         $context->replace($this, $resultL || $resultR);
+     }
+}
+
+class AndExpression extends OperatorExpression{
+     protected function doInterpret(InterpreterContext $context, $resultL, $resultR){
+         $context->replace($this, $resultL && $resultR);
+     }
+}
+
 $context = new InterpreterContext();
 $literal = new LiteralExpression('quatro');
 $literal->interpret($context);
